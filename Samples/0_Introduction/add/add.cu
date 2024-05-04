6e1d55508090d7c0aa910fcbb2b67fa8277d790f
崔容培 <1962569814@qq.com>
2024-05-04 13:11:17 +0800

    001 cuda-An Even Easier Introduction to CUDA

--- Samples/0_Introduction/add/add.cu
+++ Samples/0_Introduction/add/add.cu
@@ -1,10 +1,14 @@
-﻿#include <iostream>
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <iostream>
 #include <math.h>
 // Kernel function to add the elements of two arrays
 __global__
 void add(int n, float* x, float* y)
 {
-    for (int i = 0; i < n; i++)
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for (int i = index; i < n; i += stride)
         y[i] = x[i] + y[i];
 }
 
@@ -24,7 +28,10 @@ int main(void)
     }
 
     // Run kernel on 1M elements on the GPU
-    add << <1, 1 >> > (N, x, y);
+    int blockSize = 256;
+    int numBlocks = (N + blockSize - 1) / blockSize;
+    add << <numBlocks, blockSize >> > (N, x, y);
+    
 
     // Wait for GPU to finish before accessing on host
     cudaDeviceSynchronize();
